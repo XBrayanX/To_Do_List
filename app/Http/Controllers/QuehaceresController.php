cceb1022b5b2095408f7bb2da4ba0ced83a5dcf0
Brayan <66199534+XBrayanX@users.noreply.github.com>
2023-03-04 20:46:28 -0600

    Feat(controller) Agregada la funcionalidad de Index en QuehaceresController
    - Muestra todos los datos de la tabla quehaceres
    - Funciona correctamente

--- app/Http/Controllers/QuehaceresController.php
+++ app/Http/Controllers/QuehaceresController.php
@@ -1,49 +1,43 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Quehaceres;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class QuehaceresController extends Controller
-{
+class QuehaceresController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index() {
+        return DB::select('SELECT * from quehaceres');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Quehaceres $quehaceres)
-    {
+    public function show(Quehaceres $quehaceres) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Quehaceres $quehaceres)
-    {
+    public function update(Request $request, Quehaceres $quehaceres) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Quehaceres $quehaceres)
-    {
+    public function destroy(Quehaceres $quehaceres) {
         //
     }
 }
