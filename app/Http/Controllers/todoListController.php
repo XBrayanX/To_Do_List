01f5f2f9d8c89f4b6d42496fcbb6c637bd4ef728
Brayan Aguilar <66199534+XBrayanX@users.noreply.github.com>
2024-05-25 22:10:20 -0600

    Refactor + improve code
    - Using eloquent instead of DB for faster maintenance
    - reduce code and organize

--- app/Http/Controllers/todoListController.php
+++ app/Http/Controllers/todoListController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\todoList;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -13,136 +14,118 @@ class todoListController extends Controller {
      * Display a listing of the resource.
      */
     public function index() {
-        $data = DB::select('SELECT * from todolist');
-
-        return $this->response_api($data);
+        return $this->response_api( todoList::all() );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) {
-        //Validaciones
+    public function store( Request $request ) {
+        // Validaciones
         $this->validate_name();
         $this->validate_deadline();
-        $this->validator = Validator::make($request->all(), $this->validator_rules);
+        $this->validator = Validator::make( $request->all(), $this->validator_rules );
 
-        if ($this->validator->fails()) {
-            return $this->response_api(null, 400);
+        if ( $this->validator->fails() ) {
+            return $this->response_api( null, 400 );
         }
 
-        //cambiar formato de la fecha para poder ser Insertada en Mysql
-        $deadline = $this->convert_data($request->deadline);
+        // cambiar formato de la fecha para poder ser Insertada en Mysql
+        $deadline = $this->convert_date( $request->deadline );
 
-        DB::insert('INSERT into todolist(name, deadline)
-        values(?, ?)', [$request->name, $deadline]);
+        $record = todoList::create( [
+            'name'     => $request->name,
+            'deadline' => $deadline
+        ]);
 
-        $last_id = DB::select('SELECT id from todolist
-        order by id desc limit 1');
-
-        return $this->response_api($last_id, 201);
+        return $this->response_api( ['id' => $record->id], 201 );
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Request $request) {
-        //Validaciones
-        $this->validate_id($request);
-        $this->validator = Validator::make($request->all(), $this->validator_rules);
+    public function show( Request $request ) {
+        // Validaciones
+        $this->validate_id( $request );
+        $this->validator = Validator::make( $request->all(), $this->validator_rules );
 
-        if ($this->validator->fails()) {
-            return $this->response_api(null, 400);
+        if ( $this->validator->fails() ) {
+            return $this->response_api( null, 400 );
         }
 
-        $data = DB::select('SELECT * from todolist where id = ? limit 1', [$request->id]);
-        return $this->response_api($data);
+        return $this->response_api( todoList::find( $request->id ) );
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request) {
-        //Validaciones
+    public function update( Request $request ) {
+        // Validaciones
         $this->validate_id();
-        $this->validate_name('sometimes');
-        $this->validate_deadline('sometimes');
+        $this->validate_name( 'sometimes' );
+        $this->validate_deadline( 'sometimes' );
         $this->validate_complete();
-        $this->validator = Validator::make($request->all(), $this->validator_rules);
+        $this->validator = Validator::make( $request->all(), $this->validator_rules );
 
-        if ($this->validator->fails()) {
-            return $this->response_api(null, 400);
+        if ( $this->validator->fails() ) {
+            return $this->response_api( null, 400 );
         }
 
-        $data = [
-            'name'     => $request->name ?? null,
-            'deadline' => $request->deadline ?? null,
-            'complete' => $request->complete ?? null
-        ];
+        $data = array_filter( $request->all() );
 
-        //Eliminar Valores Vacíos
-        $data = array_filter($data);
-
-        if (count($data) > 0) {
-            if (!empty($data['deadline'])) {
-                //Convertir fecha
-                $data['deadline'] = $this->convert_data($data['deadline']);
+        if ( count( $data ) > 0 ) {
+            if ( !empty( $data['deadline'] ) ) {
+                // Convertir fecha
+                $data['deadline'] = $this->convert_date( $data['deadline'] );
             }
+ 
+            $affected = todoList::where( 'id', $request->id )->update( $data );
 
-            //Crear Consulta
-            $query = $this->create_query($data);
-
-            //Ejecutar Consulta
-            $affected = DB::update("UPDATE todolist
-            set $query
-            where id = ?
-            limit 1", [$request->id]);
-
-            return $this->response_api(['affected' => $affected]);
+            return $this->response_api( ['affected' => $affected] );
         }
-        return $this->response_api(null, 400);
+        return $this->response_api( null, 400 );
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Request $request) {
+    public function destroy( Request $request ) {
         $query = '';
 
-        if (strtolower($request->option) === 'many') {
-            //Validación
-            $this->validate_many_id($request);
+        if ( strtolower( $request->option ) === 'many' ) {
+            // Validación
+            $this->validate_many_id( $request );
             $query = "where id in($request->ids)";
 
-        } elseif (strtolower($request->option) === 'all') {
+        } elseif ( strtolower( $request->option ) === 'all' ) {
             $query = '';
 
         } else {
-            //Validación
-            $this->validate_id($request);
+            // Validación
+            $this->validate_id( $request );
             $query = "where id = $request->id limit 1";
         }
 
-        if (!empty($this->validator_rules)) {
-            $this->validator = Validator::make($request->all(), $this->validator_rules);
+        if ( !empty( $this->validator_rules ) ) {
+            $this->validator = Validator::make( $request->all(), $this->validator_rules );
 
-            if ($this->validator->fails()) {
-                return $this->response_api(null, 400);
+            if ( $this->validator->fails() ) {
+                return $this->response_api( null, 400 );
             }
         }
 
-        //Ejecutar consulta
-        $affected = DB::delete("DELETE from todolist $query");
+        // Ejecutar consulta
+        $affected = DB::delete( "DELETE from todolist $query" );
 
-        return $this->response_api(['affected' => $affected]);
+        return $this->response_api( ['affected' => $affected] );
     }
 
-    //Funciones Adicionales
-    //--------------------------------------------------------------------------------------------------
-    private function response_api($data = null, int $code = 200) {
+    // Funciones Adicionales
+    // --------------------------------------------------------------------------------------------------
+    private function response_api( $data = null, int $code = 200 ) {
         $errors = null;
 
-        if (!empty($this->validator) && $this->validator->fails()) {
+        if ( !empty( $this->validator ) && $this->validator->fails() ) {
             $errors = $this->validator->messages();
         }
 
@@ -150,37 +133,30 @@ class todoListController extends Controller {
             'code'   => $code,
             'data'   => $data,
             'errors' => $errors,
-        ]; 
+        ];
 
-        //Quitar Valores vacíos
-        $response = array_filter($response);
+        // Quitar Valores vacíos
+        $response = array_filter( $response );
 
-        return response()->json($response, $code);
+        return response()->json( $response, $code );
     }
 
-    private function convert_data($date) {
-        return Carbon::createFromFormat('d-m-Y', $date)->format('Y-m-d');
+    private function convert_date( $date ) {
+        return Carbon::createFromFormat( 'd-m-Y', $date )->format( 'Y-m-d' );
     }
 
-    private function create_query(array $data): string {
-        $string = implode(',', array_map(function ($key, $value) {
-            return "$key = '$value'";
-        }, array_keys($data), $data));
 
-        return $string;
-    }
-
-    //Funciones de Validación
-    //--------------------------------------------------------------------------------------------------
+    // Funciones de Validación
+    // --------------------------------------------------------------------------------------------------
     private function validate_id(): void {
         $this->validator_rules['id'] = 'required|numeric';
     }
 
-    private function validate_name(string $require = 'required'): void {
+    private function validate_name( string $require = 'required' ): void {
         $this->validator_rules['name'] = "$require|regex:/^[#\-+\w ]+$/|min:4|max:60";
     }
 
-    private function validate_deadline(string $require = 'required'): void {
+    private function validate_deadline( string $require = 'required' ): void {
         $this->validator_rules['deadline'] = "$require|date_format:d-m-Y";
     }
 
