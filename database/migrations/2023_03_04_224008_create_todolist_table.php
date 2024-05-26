115a631042d917c46de6506c6f7c4c7acc80bf7e
Brayan Aguilar <66199534+XBrayanX@users.noreply.github.com>
2024-05-25 21:15:22 -0600

    correct some writing errors + organize the code

--- database/migrations/2023_03_04_224008_create_todolist_table.php
+++ database/migrations/2023_03_04_224008_create_todolist_table.php
@@ -4,28 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('todolist', function (Blueprint $table) {
+    public function up(): void {
+        Schema::create( 'todolist', function ( Blueprint $table ) {
             $table->id();
-            $table->string('name', 150);
-            $table->date('deadline');
-            $table->enum('complete', ['si', 'no'])->default('no');
-            $table->timestamp('create_at')->useCurrent();
-            $table->timestamp('update_at')->nullable()->useCurrentOnUpdate();
-        });
+            $table->string( 'name', 150 );
+            $table->date( 'deadline' );
+            $table->enum( 'complete', ['si', 'no'] )->default( 'no' );
+            $table->timestamp( 'created_at' )->useCurrent();
+            $table->timestamp( 'updated_at' )->nullable()->useCurrentOnUpdate();
+        } );
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('todolist');
+    public function down(): void {
+        Schema::dropIfExists( 'todolist' );
     }
 };
